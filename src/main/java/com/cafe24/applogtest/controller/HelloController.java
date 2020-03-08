5c54fab0358399fd6fcd76be75c8c4102517cf24
jungeunlee95 <leeap1004@gmail.com>
2020-03-08 23:29:40 +0900

    add logback spring profiles

--- src/main/java/com/cafe24/applogtest/controller/HelloController.java
+++ src/main/java/com/cafe24/applogtest/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.cafe24.applogtest.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@Slf4j
 public class HelloController {
 
     private static Logger log = LoggerFactory.getLogger(HelloController.class);
